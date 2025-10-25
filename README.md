67e1f34186eb2fa233cffc82ae0cad71924889db
parvatapuvamshi <parvathapuvamshikrishna@gmail.com>
2025-10-25 12:41:58 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sketch_to_image
+to run frontend use npm run dev 
+to run backend  "# finalsketchtoimage" 
--- project/src/App.jsx
+++ project/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
       const formData = new FormData();
       formData.append('file', file);
 
-      const response = await fetch('https://dfefe599f2c4.ngrok-free.app/generate_json', {
+      const response = await fetch('https://516d4430e86b.ngrok-free.app/generate_json', {
         method: 'POST',
         body: formData,
       });
